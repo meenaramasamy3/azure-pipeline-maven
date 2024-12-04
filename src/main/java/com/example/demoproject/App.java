26ed86490828e1da0ef80f50890676fd3d78da70
SatheshKumarPalanisa <SatheshKumarPalanisa@IBM-PF4CPX1W>
2024-12-04 15:07:18 -0500

    initial commit

---
+++ src/main/java/com/example/demoproject/App.java
@@ -0,0 +1,11 @@
+package com.example.demoproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Test1");
+    }
+}
